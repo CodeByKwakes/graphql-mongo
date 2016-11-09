c1b991239222015ec88561c4277ed097b3a2dfde
Kwaku Prempeh <kwakesproject@gmail.com>
2016-11-09 21:52:08 +0000

    imported cors and added babelrc file

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from 'express';
+import cors from 'cors'
 import graphqlHTTP from 'express-graphql';
 import mongoose from 'mongoose';
 
-const app = express()
+const server = express()
+server.use(cors())
 
-app.use('/spont', graphqlHTTP(req => ({
+server.use('/spont', graphqlHTTP(req => ({
   schema,
   pretty: true
 })));
 
 mongoose.connect('mongodb://localhost/spontDB');
 
-const server = app.listen(8080, () => {
+server.listen(8080, () => {
   console.log('Listening at port', server.address().port)
 })
